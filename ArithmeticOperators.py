05d7843426452d9813349c0137af43a278cdb0ac
tahirtall <tahirtall46@gmail.com>
2020-03-02 01:12:23 -0600

    Challenge Completed

---
+++ ArithmeticOperators.py
@@ -0,0 +1,28 @@
+''' 
+Task:
+Read two integers from STDIN and print three lines where:
+    1) The first line contains the sum of the two numbers.
+    2) The second line contains the difference of the two numbers (first - second).
+    3) The third line contains the product of the two numbers.
+
+Sample Input:
+
+    3
+    2
+
+Sample Output:
+
+    5
+    1
+    6
+
+'''
+if __name__ == '__main__':
+    #a = int(input())
+    #b = int(input())
+    a = 3
+    b = 2
+
+print(a+b)
+print(a-b)
+print(a*b)
