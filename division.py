9d130678bd190038365c6df5ff412a26d0286233
tahirtall <tahirtall46@gmail.com>
2020-03-02 01:19:02 -0600

    Challenge Completed

---
+++ division.py
@@ -0,0 +1,27 @@
+'''
+Task:
+
+Read two integers and print two lines. The first line should contain integer division, a // b. The second line should contain float division,  a/ b.
+You don't need to perform any rounding or formatting operations.
+
+Sample Input:
+
+    4
+    3
+
+Sample Output:
+
+    1
+    1.333333333
+
+'''
+
+if __name__== '__main__':
+    #a = (input())
+    #b = (input())
+    a = 6564424525
+    b = 323252462
+
+print(a//b)
+print(float(a)/float(b))
+
