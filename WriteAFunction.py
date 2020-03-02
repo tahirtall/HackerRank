307dec6427812df80ec13a788b903e70729c6f1f
tahirtall <tahirtall46@gmail.com>
2020-03-02 01:47:40 -0600

    Challenge Completed

--- WriteAFunction.py
+++ WriteAFunction.py
@@ -4,7 +4,7 @@ In the Gregorian calendar three criteria must be taken into account to identify 
 
 The year can be evenly divided by 4, is a leap year, unless:
 The year can be evenly divided by 100, it is NOT a leap year, unless:
-The year is also evenly divisible by 400. Then it is a leap year.
+    The year is also evenly divisible by 400. Then it is a leap year.
 This means that in the Gregorian calendar, the years 2000 and 2400 are leap years, while 1800, 1900, 2100, 2200, 2300 and 2500 are NOT leap years.Source
 
 Task
@@ -32,9 +32,13 @@ Sample Output 0
 def is_leap(year):
     leap = False
     
-    # Write your logic here
-    
+    if year%4==0:
+        leap = True
+        if year%100==0: 
+            leap = False
+            if year%400==0:
+                leap = True
     return leap
 
-year = int(input())
+year = 2300
 print(is_leap(year))
