1492d4fc7aee1f3d1824de49c02c908f1980c9d1
Tahir Uzun <tahirtall46@gmail.com>
2020-03-08 03:09:00 -0500

    New Challenge

---
+++ SockMerchant.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class SockMerchant {
+
+    // Complete the sockMerchant function below.
+    static int sockMerchant(int n, int[] ar) {
+        int counter = 0;
+        int result = 1;
+        Arrays.sort(ar);
+        for (int i=1; i<n; i++) {
+            if(ar[i] == ar[i-1]) {
+                counter++;
+                if(counter%2==0) {
+                    result++;
+                }
+            }
+        }
+        return result;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        int[] arr = {
+        44, 55, 11, 15, 4, 72, 26, 91, 80, 14, 43, 78, 70, 75, 36, 83, 78, 91, 17, 17, 54, 65, 60, 21, 58, 98, 87, 45, 75, 97, 81, 18, 51, 43, 
+        84, 54, 66, 10, 44, 45, 23, 38, 22, 44, 65, 9, 78, 42, 100, 94, 58, 5, 11, 69, 26, 20, 19, 64, 64, 93, 60, 96, 10, 10, 39, 94, 15, 4, 
+        3, 10, 1, 77, 48, 74, 20, 12, 83, 97, 5, 82, 43, 15, 86, 5, 35, 63, 24, 53, 27, 87, 45, 38, 34, 7, 48, 24, 100, 14, 80, 54
+        };
+
+        sockMerchant(100, arr);
+    }
+}
