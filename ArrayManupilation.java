3500f37858636dfd1dc708650f31cf5071b65afb
Tahir Uzun <tahirtall46@gmail.com>
2020-03-11 07:33:39 -0500

    There is an issue with line 24

--- ArrayManupilation.java
+++ ArrayManupilation.java
@@ -21,16 +21,18 @@ class ArrayManupilation {
                 result[x] = result[x] + myArr[k][x];
             }
         }
+        // sort result array and return it.
         Arrays.sort(result);
         return result[queries[0][0]-1];
     }
     public static void main(String[] args) {
         // input queries
         int[][] arr2D = {
-            {10, 3},
-            {1, 5, 3},
-            {4, 8, 7},
-            {6, 9, 1}   
+            {10, 4},
+            {2, 6, 8},
+            {3, 5, 7},
+            {1, 8, 1},
+            {5, 9, 15}   
         };
         // input n
         int num = 10;
