da60d437c9f8f4af8b6ef978d87d264523ae25cc
Tahir Uzun <tahirtall46@gmail.com>
2020-03-11 06:49:54 -0500

    filled the subarrays with 1

--- ArrayManupilation.java
+++ ArrayManupilation.java
@@ -1,13 +1,21 @@
+import java.util.Arrays;
 class ArrayManupilation {
     // n - number of elements in the array
     // queries - a two dimensional array of queries where each queries[i] contains three integers, a, b, and k.    
     public static int arrayManupilation(int n, int[][] queries) {
+        // initiated a new 2D array with dimentions from queries[0]
         int[][] myArr = new int[queries[0][1]][queries[0][0]];
+        // filled the subarrays with 0s
+        for (int i=0; i<queries[0][1]; i++) {
+            Arrays.fill(myArr[i], 0, queries[0][0], 0);
+        }
+        System.out.print(myArr[0][5] + " ");
         return 1;
     }
     public static void main(String[] args) {
         // input queries
         int[][] arr2D = {
+            {10, 3},
             {1, 5, 3},
             {4, 8, 7},
             {4, 9, 1}   
