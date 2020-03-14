afcb6dec3eb47b0f10b337d3522561ef2261eb95
Tahir Uzun <tahirtall46@gmail.com>
2020-03-14 01:06:10 -0500

    This is gonna be a hard one :(

---
+++ TheFundRaisingProblem.java
@@ -0,0 +1,23 @@
+class TheFundRaisingProblem {
+
+    static void guestTable(int[] generosities) {
+
+    }
+
+    static void solve() {
+
+
+    }
+    public static void main (String[] main) {
+        Integer[][] test1 = {
+            {3, 3, 3},
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9},
+            {3, 10, 20, 30},
+            {3, 40, 50, 60},
+            {3, 70, 80, 90},
+            {3}
+        };
+    }
+}
