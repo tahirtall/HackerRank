0088db1a2ca3a7f964a5bc3a656eda2a1560c94f
Tahir Uzun <tahirtall46@gmail.com>
2020-03-19 01:31:47 -0500

    scanner created, created array, and set up the pattern

--- tagContent.java
+++ tagContent.java
@@ -1,4 +1,21 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class tagContent {
+
+    public static String printHTML(String str) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        Pattern pattern = Pattern.compile("<[^>]*>");
+        Matcher m = pattern.matcher(str);
+        
+        while(m.find()) {
+            
+        }
+        return "1";
+    }
     public static void main(String[] main) {
         
     }
