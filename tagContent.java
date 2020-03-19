f08e396bd73156d136d31a5ac12ff9d8accaf937
Tahir Uzun <tahirtall46@gmail.com>
2020-03-19 16:59:23 -0500

    fixed array iteration

--- tagContent.java
+++ tagContent.java
@@ -2,17 +2,16 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
-import java.util.regex.MatchResult;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 class tagContent {
-    public static void printHTML(int n) {
+    static void printHTML() {
         Scanner scanner = new Scanner(System.in);
+        int testCases = Integer.parseInt(scanner.nextLine());
         boolean isValid = true;
-        int counter = 0;
         List<String> result = new ArrayList<String>();
-        while (counter < n) {
+        while (testCases > 0) {
             List<String> tags = new ArrayList<String>();
             String str = scanner.nextLine();
             Pattern pattern = Pattern.compile("<[^>]*>");
@@ -22,17 +21,30 @@ class tagContent {
                 tags.add(strTags);
             }
             tags.add("end");
-            for (int i=0; i<tags.size(); i=i+2) {
-                if(tags.get(i) == "end") {
+
+            for (int i=0; i<tags.size(); i++) {
+                if (tags.get(i+1) == "end") {
                     break;
                 }
-                String compare1 = tags.get(i).substring(1,(tags.get(i).length()-1));
-                String compare2 = tags.get(i+1).substring(2,(tags.get(i+1).length()-1));
-                if(!compare2.equals(compare1)) {
-                    isValid = false;
-                    break;
+                boolean slash = tags.get(i).contains("/");
+                if(slash == false) {
+                    for (int ii=i+1; ii<tags.size(); ii++) {
+                        boolean slash2 = tags.get(ii).contains("/");
+                        if (slash2 == true) {
+                            String compare1 = tags.get(i).substring(1,(tags.get(i).length()-1));
+                            String compare2 = tags.get(ii).substring(2,(tags.get(ii).length()-1));
+                            if (compare1.equals(compare2)) {
+                                tags.remove(i);
+                                tags.remove(ii);
+                            }
+                        }
+                    }
                 }
             }
+            if (tags.size() > 1) {
+                isValid = false;
+            }
+
             List<String> posArr = new ArrayList<String>();
             if (isValid == false) {
                 result.add("none");
@@ -47,13 +59,16 @@ class tagContent {
                     }
                 }
             }
-            counter++;
+            testCases--;
         }
         for (int k=0; k<result.size(); k++) {
             System.out.println(result.get(k));
         } 
     }
     public static void main(String[] main) {
-        printHTML(2);
+        //printHTML();
+        String aa = "</h1>";
+        String bb = aa.substring(1, (aa.length()-1));
+        System.out.print(aa.contains(bb));
     }
 }
