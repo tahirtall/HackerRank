e102f68e02fad9d4b646532c4e3250e8802fc55a
Tahir Uzun <tahirtall46@gmail.com>
2020-03-19 03:39:36 -0500

    can't eliminate whitespace in the array

--- tagContent.java
+++ tagContent.java
@@ -1,22 +1,38 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.regex.MatchResult;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class tagContent {
 
-    public static String printHTML(String str) {
+     public static void printHTML() {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<Integer> arr = new ArrayList<Integer>();
-        Pattern pattern = Pattern.compile("<[^>]*>");
-        Matcher m = pattern.matcher(str);
-        
-        while(m.find()) {
-            
+        int n = scanner.nextInt();
+        ArrayList<Integer> outputArr = new ArrayList<Integer>();
+        List<String> posArr = new ArrayList<String>();
+        for (int i=0; i<n; i++) {
+            String str = scanner.nextLine();
+            Pattern pattern = Pattern.compile("<[^>]*>");
+            Matcher m = pattern.matcher(str);
+            boolean b = m.matches();
+            if (b == true) {
+                String[] splitArr = str.split("<[^>]*>");
+                posArr = Arrays.asList(splitArr);
+            }
+            if (b == false) {
+                posArr.add("invalid");
+            }
         }
-        return "1";
-    }
+        for (int ii=0; ii<posArr.size(); ii++) {
+            if (posArr.get(ii).length() > 1) {
+                System.out.println(posArr.get(ii));
+            }
+        }
+    } 
     public static void main(String[] main) {
-        
+        printHTML();
     }
 }
