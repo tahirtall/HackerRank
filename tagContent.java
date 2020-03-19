9406ca67f77b44c49109c5da5e66970ddf4d3a86
Tahir Uzun <tahirtall46@gmail.com>
2020-03-19 07:59:33 -0500

    fixed tagsArr iteration

--- tagContent.java
+++ tagContent.java
@@ -8,34 +8,46 @@ import java.util.regex.Pattern;
 
 public class tagContent {
 
-     public static void printHTML() {
-        Scanner scanner = new Scanner(System.in);
-        String str = scanner.nextLine();
-        boolean matched = false;
-
+    public static final Scanner scanner = new Scanner(System.in);
+    public static void printHTML(int n) {
+        boolean matched = true;
+        String strTags;
+        List<String> result = new ArrayList<String>();
         List<String> posArr = new ArrayList<String>();
         List<String> tags = new ArrayList<String>();
-        Pattern pattern = Pattern.compile("<[^>]*>");
-        Matcher matcher = pattern.matcher(str);
-        while(matcher.find()) {
-            String strTags = matcher.group();
-            tags.add(strTags);
+        int counter = 0;
+        while (counter != n) {
+            String str = scanner.nextLine();
+            Pattern pattern = Pattern.compile("<[^>]*>");
+            Matcher matcher = pattern.matcher(str);
+            while(matcher.find()) {
+                strTags = matcher.group();
+                tags.add(strTags);
+            }
+            tags.add("end");
+
+
+            if (matched == false) {
+                result.add("none");
+            }
+            else {
+                String[] splitRegex = str.split("<[^>]*>");
+                posArr = Arrays.asList(splitRegex);
+                for (int j=0; j<posArr.size(); j++) {
+                    posArr.get(j).trim();
+                    if (posArr.get(j).length() > 1) {
+                        result.add(posArr.get(j));
+                    }
+                }
+            }
+            counter++;
         }
-        tags.add("end");
-        for (int i=0; i<tags.size(); i=i+2) {
-           if(tags.get(i+2) == "end")   break;
-           if (tags.get(i).equals(tags.get(i+1)))    matched = true;
-           else                                 matched = false;
+        for (int k=0; k<result.size(); k++) {
+            System.out.print(result.get(k));
         }
-
-        System.out.print(matched);
-
-
-        /* String[] result = str.split("\\<.*?\\>");
-        posArr = Arrays.asList(result);
-        System.out.print(posArr + "\n"); */
-    } 
+    }
     public static void main(String[] main) {
-        printHTML();
+        String a = "123456";
+        System.out.print(a.substring(1,5));
     }
 }
