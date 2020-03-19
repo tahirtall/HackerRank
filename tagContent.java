6b273cf118a69110a9c91ac5f960bdff6e8ecab2
Tahir Uzun <tahirtall46@gmail.com>
2020-03-19 05:55:12 -0500

    typo checking completed

--- tagContent.java
+++ tagContent.java
@@ -10,11 +10,30 @@ public class tagContent {
 
      public static void printHTML() {
         Scanner scanner = new Scanner(System.in);
-        List<String> posArr = new ArrayList<String>();
         String str = scanner.nextLine();
-        String[] result = str.split("\\<.*?\\>");
+        boolean matched = false;
+
+        List<String> posArr = new ArrayList<String>();
+        List<String> tags = new ArrayList<String>();
+        Pattern pattern = Pattern.compile("<[^>]*>");
+        Matcher matcher = pattern.matcher(str);
+        while(matcher.find()) {
+            String strTags = matcher.group();
+            tags.add(strTags);
+        }
+        tags.add("end");
+        for (int i=0; i<tags.size(); i=i+2) {
+           if(tags.get(i+2) == "end")   break;
+           if (tags.get(i).equals(tags.get(i+1)))    matched = true;
+           else                                 matched = false;
+        }
+
+        System.out.print(matched);
+
+
+        /* String[] result = str.split("\\<.*?\\>");
         posArr = Arrays.asList(result);
-        System.out.print(posArr + "\n");
+        System.out.print(posArr + "\n"); */
     } 
     public static void main(String[] main) {
         printHTML();
