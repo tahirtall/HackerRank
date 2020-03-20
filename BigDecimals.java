d592bddc6854d57018ea4f9c443069dc749d4641
Tahir Uzun <tahirtall46@gmail.com>
2020-03-20 09:59:39 -0500

    so close to finishing it, but need Aaron's help

--- BigDecimals.java
+++ BigDecimals.java
@@ -11,10 +11,24 @@ public class BigDecimals {
         for(int i=0;i<n;i++){
             s[i]=sc.next();
         }
-      	sc.close();
+        sc.close();
 
-        //Write your code here
-
+        for(int j=0; j<s.length; j++) {
+            if (s[j+1] == null) {
+                break;
+            }
+            for (int jj=j; j<s.length; jj++) {
+                if (s[jj] == null) {
+                    break;
+                }
+                BigDecimal currJ = new BigDecimal(s[j]);
+                BigDecimal currJJ = new BigDecimal(s[jj]);
+                if (currJJ.compareTo(currJ) > 0) {
+                    s[j] = currJJ.toString();
+                    s[jj] = currJ.toString();
+                }
+            }
+        }
         //Output
         for(int i=0;i<n;i++)
         {
